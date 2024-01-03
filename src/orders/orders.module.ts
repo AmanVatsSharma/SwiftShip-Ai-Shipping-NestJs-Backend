c18c3a28378a49f8cbf361310a2deb914735cba5
AmanVatsSharma <aman95026@gmail.com>
2024-01-03 10:43:04 +0530

    fix: add missing OrdersModule and wire up providers (oops, this was left out in initial scaffold)

---
+++ src/orders/orders.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { OrdersResolver } from './orders.resolver';
+import { OrdersService } from './orders.service';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Module({
+  providers: [OrdersResolver, OrdersService, PrismaService],
+})
+export class OrdersModule {} 
